22af47cd5c4ee1d3e77c7c3c1b283caab87be2b9
Garrett <Garrett@YUP>
2024-06-04 13:39:24 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment5._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lastWord = {"Hello World" };
+            string space = " ";
+
+            for (int i = 0; i - 1 < lastWord.Length; i++)
+            {
+                string[] words = lastWord[i].Split(space.ToCharArray());
+
+                string last = words[words.Length - 1];
+
+                int length = last.Length;
+
+                Console.WriteLine($"{length} is the length of the last word.");
+            }
+        }
+    }
+}
